798a2eb63d45cec257202f6f11dfa853cdd543bf
kaisbad <158204041+kaisbad@users.noreply.github.com>
2024-10-01 12:29:19 -0400

    a

---
+++ index.js
@@ -0,0 +1,21 @@
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  descriptiom: document.querySelector(".card-desc"),
+  items: document.querySelector("li"),
+};
+
+console.log(DOMSelectors.items);
+function changeColor() {
+  //find element with querySelector
+  let button = document.querySelector("button");
+  //listen for a click even
+  console.log(button);
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    //logging the click event
+    console.log(event.target);
+    button.style.backgroundColor = "red";
+  });
+}
+//call functions
+changeColor();
